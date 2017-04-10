27ec3390e6143c39ac2f82b4c8b44eb6910da3d3
Vasily <Vasily@HOME-PC>
2017-04-10 19:10:43 +0300

    Msx frequency don't rely on framework limit anymore, srd::chrono use instead

--- TermPaper/Controller.cpp
+++ TermPaper/Controller.cpp
@@ -1,6 +1,7 @@
 #include "Controller.h"
 #include "WinSingleton.h"
 #include <iostream>
+#include <chrono>
 
 Controller::Controller()
 {
@@ -16,55 +17,59 @@ void Controller::NotifyModel(Events _event)
 
 void Controller::observe()
 {
+	std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+	std::chrono::duration<double> frequency(0.001);
 	NotifyModel(LoadLevel);
 	viewer.handleViewerEvent(LevelCreated);
 	sf::Event event;
 	window->setKeyRepeatEnabled(false); // To disable repeated KeyPressed events
 	while (window->isOpen())
 	{
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) model.handleEvent(HeroMoveLeft);
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) model.handleEvent(HeroMoveRight);
-		if (window->pollEvent(event))
+		if (std::chrono::system_clock::now() - start > frequency)
 		{
-			switch (event.type)
+			start = std::chrono::system_clock::now();
+			if (window->pollEvent(event))
 			{
-			case sf::Event::Closed:
-				window->close();
-				break;
-			case sf::Event::KeyPressed:
-				switch (event.key.code)
+				switch (event.type)
 				{
-				case sf::Keyboard::Left:
-					model.handleEvent(HeroMoveLeft);
-					break;
-				case sf::Keyboard::Right:
-					model.handleEvent(HeroMoveRight);
-					break;
-				case sf::Keyboard::Up:
-					model.handleEvent(Jumping);
-					break;
-				case sf::Keyboard::Escape:
+				case sf::Event::Closed:
 					window->close();
 					break;
+				case sf::Event::KeyPressed:
+					switch (event.key.code)
+					{
+					case sf::Keyboard::Left:
+						model.handleEvent(HeroMoveLeft);
+						break;
+					case sf::Keyboard::Right:
+						model.handleEvent(HeroMoveRight);
+						break;
+					case sf::Keyboard::Up:
+						model.handleEvent(Jumping);
+						break;
+					case sf::Keyboard::Escape:
+						window->close();
+						break;
+					}
+					break;
+				case sf::Event::KeyReleased:
+					switch (event.key.code)
+					{
+					case sf::Keyboard::Left:
+						model.handleEvent(StopHeroMoveLeft);
+						break;
+					case sf::Keyboard::Right:
+						model.handleEvent(StopHeroMoveRight);
+						break;
+					case sf::Keyboard::Up:
+						model.handleEvent(StopHero);
+						break;
+					}
+					break;
 				}
-				break;
-			case sf::Event::KeyReleased:
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Left:
-					model.handleEvent(StopHeroMoveLeft);
-					break;
-				case sf::Keyboard::Right:
-					model.handleEvent(StopHeroMoveRight);
-					break;
-				case sf::Keyboard::Up:
-					model.handleEvent(StopHero);
-					break;
-				}
-				break;
 			}
+			model.handleEvent(Move);
+			viewer.handleViewerEvent(Update);
 		}
-		model.handleEvent(Move);
-		viewer.handleViewerEvent(Update);
 	}
 }
